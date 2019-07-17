b9006dbf7fbb0647339be12d21a4dbb00cdfb007
The-Totti <52722890+The-Totti@users.noreply.github.com>
2019-07-17 20:57:20 -0300

    Achar Número

---
+++ fase04/ex07/acharNumero.py
@@ -0,0 +1,9 @@
+def acharNumero(num1):
+    if num1 < 5:
+        return ("a")
+    elif num1 <= 10:
+        return ("b")
+    elif num1 <= 100:
+        return ("c")
+    elif num1 > 100:
+        return ("d")
