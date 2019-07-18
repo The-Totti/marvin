0cec2a674cbbf87d3fcf9a752427ec5c10f05c77
The-Totti <52722890+The-Totti@users.noreply.github.com>
2019-07-17 21:22:19 -0300

    Update abc.py

--- fase04/ex04/abc.py
+++ fase04/ex04/abc.py
@@ -1,5 +1,5 @@
 def abc(palavra):
-    if palavra[0:1] == "a" or palavra[0:1] == "b" or palavra[0:1] == "c":
+    if palavra[0:1] == "a" or palavra[0:1] == "b" or palavra[0:1] == "c" or palavra[0:1] == "A" or palavra[0:1] == "B" or palavra[0:1] == "C":
        return ("True")
     else:
         return ("False")
