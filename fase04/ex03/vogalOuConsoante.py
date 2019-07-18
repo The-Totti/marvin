3a14ad912ad9bef294d15e1a46a08253e589f12a
The-Totti <52722890+The-Totti@users.noreply.github.com>
2019-07-18 20:26:41 -0300

    Update vogalOuConsoante.py

--- fase04/ex03/vogalOuConsoante.py
+++ fase04/ex03/vogalOuConsoante.py
@@ -1,5 +1,5 @@
 def vogalOuConsoante(letra):
-    if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+    if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u" or letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U" :
        return ("Vogal")
     else:
         return ("Consoante")
