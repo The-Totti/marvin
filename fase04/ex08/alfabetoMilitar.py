aa886feaf90d9218c0860380323129e108175e7d
The-Totti <52722890+The-Totti@users.noreply.github.com>
2019-07-17 21:06:34 -0300

    Alfabeto Militar

---
+++ fase04/ex08/alfabetoMilitar.py
@@ -0,0 +1,12 @@
+def alfabetoMilitar(letra):
+    if letra == "a":
+        return ("alpha")
+    elif letra == "e":
+        return ("echo")
+    elif letra == "i":
+        return ("india")
+    elif letra == "o":
+        return ("oscar")
+    elif letra == "u":
+        return ("uniform")
+        
